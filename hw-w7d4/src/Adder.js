609ab9f882fba20deb95fb906e301241b4db4a3f
lmdsultan <mdgetbetter.gmail.com>
2020-01-01 19:46:45 +0300

    W7D4

---
+++ hw-w7d4/src/Adder.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+let num1 = 3
+let num2 = 3
+function Adder(){
+    return (<p>The sum of {num1} and {num2} is {num1+num2}</p>)
+}
+export default Adder
---
+++ hw-w7d4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Adder from './Adder'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home Work Week 7 Day 4</h1>
+      <Adder/>
+    </div>
+  );
+}
+
+export default App;
