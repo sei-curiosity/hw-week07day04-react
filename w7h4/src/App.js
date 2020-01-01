82f2f6c162e2cd7fe3b6fccffe399cadbc6a32b3
roush55 <ranaabumelha@gmail.com>
2020-01-02 01:55:02 +0300

    w7hw4

---
+++ w7h4/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    //var num1 = <input id="number" type="number"  />;
+  //var num2 =<input id="number2" type="number" />;
+   var num1 ;
+   var num2;
+   var totel;
+   
+  return (
+    <div className="App">
+      <h1>Math</h1>
+  <p>First-Number={num1= 5} <br/>Second-Number={num2= 10 }<br/>
+  totel:
+  {totel =num1+num2}
+        </p>
+    </div>
+  );
+}
+
+export default App;
