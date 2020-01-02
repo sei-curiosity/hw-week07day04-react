826da682a50792a1a1c6e7e0b8fdccfaa74eb871
Abdulah68 <abdulahali6565@gmail.com>
2020-01-02 06:52:41 +0300

    solved hw-week07day4

--- hw-w7d4/src/App.js
+++ hw-w7d4/src/App.js
@@ -5,13 +5,32 @@ import logo from './logo.svg';
 let num1  = 2
 let num2 = 4
 
+function Adder(){
+
+  return(
+    <div>
+
+
+<h1>The sum of {num1}  and  {num2} is {num1 + num2}</h1>
+
+    </div>
+
+
+  );
+
+
+
+}
+
+
 function App() {
   return (
     <div className="Adder">
-<h1>The sum of {num1}  and  {num2} is {num1 + num2}</h1>
+
       
     </div>
   );
 }
+export default Adder;
 
-export default App;
+// export default App;
