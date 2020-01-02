eeff83d51e214a76afdede6b24db9de24a2cbd4f
Abdulah68 <abdulahali6565@gmail.com>
2020-01-02 06:45:54 +0300

    solve hw-week07day4

---
+++ hw-w7d4/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+let num1  = 2
+let num2 = 4
+
+function App() {
+  return (
+    <div className="Adder">
+<h1>The sum of {num1}  and  {num2} is {num1 + num2}</h1>
+      
+    </div>
+  );
+}
+
+export default App;
