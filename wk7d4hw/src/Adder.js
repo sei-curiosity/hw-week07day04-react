7e547f0e8fdf4c994d7b08ff93181aba14454e51
leenahabi <leenaalnahabi@gmail.com>
2020-01-02 08:41:44 +0300

    wk7d4hw

---
+++ wk7d4hw/src/Adder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+    
+let Adder = function() {
+    let num1 =4
+    let num2=6
+    return ( <p>The sum of {num1} and {num2} is {num1+num2}</p> );
+  }
+  
+  export default Adder;
---
+++ wk7d4hw/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Adder from './Adder.js';
+
+function App() {
+  return ( <div className="App">
+          <h3>Hello welcome to Adder</h3>
+          <Adder/>
+          </div>);
+}
+
+export default App;
